92aea8b23bc44e0cf725e31206ad5a5350314c5f
DanielKoyote <alencar.gestao@gmail.com>
2023-02-25 17:41:25 +0000

    testado

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,141 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const Calc());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Calc extends StatefulWidget {
+  const Calc({Key? key}) : super(key: key);
+  _CalcState createState() => _CalcState();
+}
+
+class _CalcState extends State<Calc> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Calc'),
+          backgroundColor: Colors.red,
         ),
+        body:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'Número',
+                style: TextStyle(
+                  fontSize: 72,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                'AC',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '<x',
+                style: TextStyle(fontSize: 48),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                '7',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '8',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '9',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '/',
+                style: TextStyle(fontSize: 48),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                '4',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '5',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '6',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                'x',
+                style: TextStyle(fontSize: 48),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                '1',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '2',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '3',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '-',
+                style: TextStyle(fontSize: 48),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                '0',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                ',',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '=',
+                style: TextStyle(fontSize: 48),
+              ),
+              Text(
+                '+',
+                style: TextStyle(fontSize: 48),
+              ),
+            ],
+          ),
+          Text('coluna 3'),
+        ]),
       ),
     );
   }
